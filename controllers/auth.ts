d69e56d6310507ebed395a82c84003941daaad18
Arbnor <arbnorlumezi@gmail.com>
2022-01-06 21:56:50 +0100

    deploy

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -10,7 +10,7 @@ AuthRouter.get(
   '/auth/facebook/callback',
   passport.authenticate('facebook'),
   (req, res) => {
-    console.log('🚀 ~ file: auth.ts ~ line 13 ~ req', req);
+    logger.info('🚀 ~ file: auth.ts ~ line 13 ~ req', req);
     res.redirect('/');
   }
 );
