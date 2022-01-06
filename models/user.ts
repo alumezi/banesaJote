c8ed612aa48b2ab506e4765c9f7594361ceb4479
Arbnor <arbnorlumezi@gmail.com>
2022-01-06 21:25:09 +0100

    deploy

--- models/user.ts
+++ models/user.ts
@@ -29,14 +29,14 @@ const userSchema = new Schema({
   ],
 });
 
-userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-    delete returnedObject.passwordHash;
-  },
-});
+// userSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//     delete returnedObject.passwordHash;
+//   },
+// });
 
 userSchema.plugin(uniqueValidator);
 
